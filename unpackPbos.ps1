c0662dbd222dd148edcb01c3641dc54d194ded76
Indigo <indifox926@gmail.com>
2021-03-24 08:45:16 -0400

    adds db

---
+++ unpackPbos.ps1
@@ -0,0 +1,194 @@
+<#
+
+.SYNOPSIS
+
+This script uses PBOManager v1.4 beta's console implementation to quickly unpack all PBOs in the working directory.
+
+.DESCRIPTION
+
+3rd Party Tools Used: 
+	- PBOManager <https://www.armaholic.com/page.php?id=16369>
+
+This script is meant to ease bulk operations when a set of PBOs need to be unpacked into folders matching the PBO name. It's useful for mod/addon development as well as unpacking multiple missions at once.
+
+__How to Use:__
+
+	1. Install PBOManager.
+	_Running the script will automatically offer you a download link if not already installed_
+
+	2. Place this script into the folder containing the PBOs to unpack.
+
+	3. Left-click the script to select it, then Right-click it and select Run with Powershell.
+	_If you don't see the Run with Powershell option, check your default file associations for .ps1 files_
+
+.NOTES
+
+Script Process:
+	1. Gets all PBOs in the current working directory.
+	2. Launches PBOManager for each PBO and instructs it to unpack into a folder with the same name (minus ".pbo").
+	_If a PBO already exists by the same name, it will back that one up first._
+
+Author: IndigoFox
+Website: indifox.info
+Email: indifox926@gmail.com
+Discord: IndigoFox#6290
+
+#>
+
+$Host.UI.RawUI.WindowTitle = "Unpack PBOs Utility"
+
+# Import PresentationFramework for message boxes
+Add-Type -AssemblyName PresentationFramework
+
+
+try {
+
+	# Check if PBOManager is installed via Registry
+	Get-Item -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Assemblies\C:|Program Files|PBO Manager v.1.4 beta|PBOConsole.exe' -EA Stop | Out-Null
+} catch {
+
+	# If not installed, prompt to download and offer link
+	if (([System.Windows.MessageBox]::Show(
+				"You must install PBOManager 1.4 beta (x64) in order to use this script. The download link has been opened in your default browser.`n`nPress OK to open the Armaholic link in your default browser.",
+				"Install Required",
+				"OKCancel",
+				"Information")) -eq "OK") {
+
+		# If user clicks OK
+		Start-Process "https://www.armaholic.com/page.php?id=16369"
+		exit
+	} else {
+
+		# If user clicks Cancel
+		exit
+	}
+}
+
+# Confirm working directory is correct
+if ([System.Windows.MessageBox]::Show(
+		"Please confirm this directory contains`nthe PBOs you'd like to unpack:`n`n$((Get-Location).Path)",
+		"Confirm Path",
+		"YesNo",
+		"Warning") -eq "No") {
+	
+	# If user clicks No, instruct and exit
+	[System.Windows.MessageBox]::Show(
+		"You will need to move this .ps1 file to the Addons directory containing the PBOs to unpack, then Right-click and Run with Powershell.`n`nIf you don't see the Run with Powershell option, check your default file associations.",
+		"Action Needed",
+		"OK",
+		"Information"
+	);
+
+	exit
+};
+
+[Array] $PBOs = Get-ChildItem -File -Filter "*.pbo" | Select-Object Name, FullName, DirectoryName
+
+ForEach ($PBO in $PBOs) {
+	Write-Host -ForegroundColor Green $PBO.Name
+	Start-Process "C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe" -ArgumentList "-unpack ""$($PBO.FullName)"" ""$($PBO.DirectoryName)\$(($PBO.Name).Substring(0, $PBO.Name.Length - 4))"""
+};
+
+
+Write-Host -ForegroundColor Green "Complete!"
+Start-Sleep 3
+
+
+# SIG # Begin signature block
+# MIIR5QYJKoZIhvcNAQcCoIIR1jCCEdICAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUs6GiQAfrDcS3edw5d3O5F4C7
+# f1yggg1QMIIDFTCCAf2gAwIBAgIQarNwqALG/45HOA0xJfnsbTANBgkqhkiG9w0B
+# AQsFADAWMRQwEgYDVQQDDAtJbmRpZ28jNjI5MDAeFw0yMTAzMTIxMDM4MThaFw0y
+# MjAzMTIxMDU4MThaMBYxFDASBgNVBAMMC0luZGlnbyM2MjkwMIIBIjANBgkqhkiG
+# 9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2qAtyT1uulJIAYesEd816AT3/QQb/J5rxx9m
+# ZWrrFXZNWDPCk887fgSRerI6pBwuq5oA7IP7llwEOZjkEo3EiZu1iqztNOwbV4MJ
+# v/Nd0MpqWHOOAATimBawUAhF+cKoKWQ7teLEEaVT4LQsNVR7WKptSv5xfTjXKbej
+# Dv94XALj9T4CMlpyFsFNG9F4WyQ1dXOrNVcqZipjT2CmEuIQKTUUkPtOxgpfKWwu
+# t7VgDdUwH7+A98Rju3Mtdvjcvv3WCc8OI17/0o0BYuVHhFAidD+aS7qXFFY5dHT+
+# WXqa6zEcv7ZnxqEoBan/I05dJ8UG70NW56rce5nW1u4AgayHPQIDAQABo18wXTAO
+# BgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwFwYDVR0RBBAwDoIM
+# aW5kaWZveC5pbmZvMB0GA1UdDgQWBBTFGaHldHGIj5xmmUwhJRWZ+eJ+tzANBgkq
+# hkiG9w0BAQsFAAOCAQEA1Het5C1AOE8jsiOD2QPsGS0ajsizu1oAE1tadRj57ePA
+# fTa0odJZXqSF+3Ch79ccPIHDq3BqFMOluiPTo0lR0iqPRXH5gdnfsQVuLZZvbjaq
+# STtP6fQKj1cNAF+QatEieqG/QudW7k8nEG23sg1r+swvTjsSe+dKncnHNesXWdg3
+# uwe7yeulrqG/q8tilDzNyJIOHpxOmcEURBp+14VQTVnAxloib/wC8g1YT5t9RJUy
+# Qh6PoiBSnW4oPwhvw5LhSaKFXCGcIrY2CSl0NJtRmnzX+VG+S84Qlij6t0u61K/c
+# TAzfbBODVKEpQeS+LSq6yMymTX2M9xxsDZj0qfgECjCCBP4wggPmoAMCAQICEA1C
+# SuC+Ooj/YEAhzhQA8N0wDQYJKoZIhvcNAQELBQAwcjELMAkGA1UEBhMCVVMxFTAT
+# BgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEx
+# MC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1cmVkIElEIFRpbWVzdGFtcGluZyBD
+# QTAeFw0yMTAxMDEwMDAwMDBaFw0zMTAxMDYwMDAwMDBaMEgxCzAJBgNVBAYTAlVT
+# MRcwFQYDVQQKEw5EaWdpQ2VydCwgSW5jLjEgMB4GA1UEAxMXRGlnaUNlcnQgVGlt
+# ZXN0YW1wIDIwMjEwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDC5mGE
+# Z8WK9Q0IpEXKY2tR1zoRQr0KdXVNlLQMULUmEP4dyG+RawyW5xpcSO9E5b+bYc0V
+# kWJauP9nC5xj/TZqgfop+N0rcIXeAhjzeG28ffnHbQk9vmp2h+mKvfiEXR52yeTG
+# dnY6U9HR01o2j8aj4S8bOrdh1nPsTm0zinxdRS1LsVDmQTo3VobckyON91Al6GTm
+# 3dOPL1e1hyDrDo4s1SPa9E14RuMDgzEpSlwMMYpKjIjF9zBa+RSvFV9sQ0kJ/SYj
+# U/aNY+gaq1uxHTDCm2mCtNv8VlS8H6GHq756WwogL0sJyZWnjbL61mOLTqVyHO6f
+# egFz+BnW/g1JhL0BAgMBAAGjggG4MIIBtDAOBgNVHQ8BAf8EBAMCB4AwDAYDVR0T
+# AQH/BAIwADAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDBBBgNVHSAEOjA4MDYGCWCG
+# SAGG/WwHATApMCcGCCsGAQUFBwIBFhtodHRwOi8vd3d3LmRpZ2ljZXJ0LmNvbS9D
+# UFMwHwYDVR0jBBgwFoAU9LbhIB3+Ka7S5GGlsqIlssgXNW4wHQYDVR0OBBYEFDZE
+# ho6kurBmvrwoLR1ENt3janq8MHEGA1UdHwRqMGgwMqAwoC6GLGh0dHA6Ly9jcmwz
+# LmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQtdHMuY3JsMDKgMKAuhixodHRwOi8v
+# Y3JsNC5kaWdpY2VydC5jb20vc2hhMi1hc3N1cmVkLXRzLmNybDCBhQYIKwYBBQUH
+# AQEEeTB3MCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wTwYI
+# KwYBBQUHMAKGQ2h0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydFNI
+# QTJBc3N1cmVkSURUaW1lc3RhbXBpbmdDQS5jcnQwDQYJKoZIhvcNAQELBQADggEB
+# AEgc3LXpmiO85xrnIA6OZ0b9QnJRdAojR6OrktIlxHBZvhSg5SeBpU0UFRkHefDR
+# BMOG2Tu9/kQCZk3taaQP9rhwz2Lo9VFKeHk2eie38+dSn5On7UOee+e03UEiifuH
+# okYDTvz0/rdkd2NfI1Jpg4L6GlPtkMyNoRdzDfTzZTlwS/Oc1np72gy8PTLQG8v1
+# Yfx1CAB2vIEO+MDhXM/EEXLnG2RJ2CKadRVC9S0yOIHa9GCiurRS+1zgYSQlT7Lf
+# ySmoc0NR2r1j1h9bm/cuG08THfdKDXF+l7f0P4TrweOjSaH6zqe/Vs+6WXZhiV9+
+# p7SOZ3j5NpjhyyjaW4emii8wggUxMIIEGaADAgECAhAKoSXW1jIbfkHkBdo2l8IV
+# MA0GCSqGSIb3DQEBCwUAMGUxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2Vy
+# dCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xJDAiBgNVBAMTG0RpZ2lD
+# ZXJ0IEFzc3VyZWQgSUQgUm9vdCBDQTAeFw0xNjAxMDcxMjAwMDBaFw0zMTAxMDcx
+# MjAwMDBaMHIxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAX
+# BgNVBAsTEHd3dy5kaWdpY2VydC5jb20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIg
+# QXNzdXJlZCBJRCBUaW1lc3RhbXBpbmcgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IB
+# DwAwggEKAoIBAQC90DLuS82Pf92puoKZxTlUKFe2I0rEDgdFM1EQfdD5fU1ofue2
+# oPSNs4jkl79jIZCYvxO8V9PD4X4I1moUADj3Lh477sym9jJZ/l9lP+Cb6+NGRwYa
+# VX4LJ37AovWg4N4iPw7/fpX786O6Ij4YrBHk8JkDbTuFfAnT7l3ImgtU46gJcWvg
+# zyIQD3XPcXJOCq3fQDpct1HhoXkUxk0kIzBdvOw8YGqsLwfM/fDqR9mIUF79Zm5W
+# YScpiYRR5oLnRlD9lCosp+R1PrqYD4R/nzEU1q3V8mTLex4F0IQZchfxFwbvPc3W
+# Te8GQv2iUypPhR3EHTyvz9qsEPXdrKzpVv+TAgMBAAGjggHOMIIByjAdBgNVHQ4E
+# FgQU9LbhIB3+Ka7S5GGlsqIlssgXNW4wHwYDVR0jBBgwFoAUReuir/SSy4IxLVGL
+# p6chnfNtyA8wEgYDVR0TAQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwEwYD
+# VR0lBAwwCgYIKwYBBQUHAwgweQYIKwYBBQUHAQEEbTBrMCQGCCsGAQUFBzABhhho
+# dHRwOi8vb2NzcC5kaWdpY2VydC5jb20wQwYIKwYBBQUHMAKGN2h0dHA6Ly9jYWNl
+# cnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcnQwgYEG
+# A1UdHwR6MHgwOqA4oDaGNGh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9EaWdpQ2Vy
+# dEFzc3VyZWRJRFJvb3RDQS5jcmwwOqA4oDaGNGh0dHA6Ly9jcmwzLmRpZ2ljZXJ0
+# LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcmwwUAYDVR0gBEkwRzA4Bgpg
+# hkgBhv1sAAIEMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNv
+# bS9DUFMwCwYJYIZIAYb9bAcBMA0GCSqGSIb3DQEBCwUAA4IBAQBxlRLpUYdWac3v
+# 3dp8qmN6s3jPBjdAhO9LhL/KzwMC/cWnww4gQiyvd/MrHwwhWiq3BTQdaq6Z+Cei
+# Zr8JqmDfdqQ6kw/4stHYfBli6F6CJR7Euhx7LCHi1lssFDVDBGiy23UC4HLHmNY8
+# ZOUfSBAYX4k4YU1iRiSHY4yRUiyvKYnleB/WCxSlgNcSR3CzddWThZN+tpJn+1Nh
+# iaj1a5bA9FhpDXzIAbG5KHW3mWOFIoxhynmUfln8jA/jb7UBJrZspe6HUSHkWGCb
+# ugwtK22ixH67xCUrRwIIfEmuE7bhfEJCKMYYVs9BNLZmXbZ0e/VWMyIvIjayS6JK
+# ldj1po5SMYID/zCCA/sCAQEwKjAWMRQwEgYDVQQDDAtJbmRpZ28jNjI5MAIQarNw
+# qALG/45HOA0xJfnsbTAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAA
+# oQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4w
+# DAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUQPGBhVkJfpP0qazGdnN6oG0m
+# TvEwDQYJKoZIhvcNAQEBBQAEggEAkUYiiLl/4KE0GbvoqslKSoDmwiOjnSy9m7hC
+# R6iTFrRguNlfXaAGHtYG9/tIuGhPaN8KRwYRHim5eyunXt0juaPHOmU6JYKShQsP
+# dYNJ9mu4XLM0DZhbRQ7RsTIfTVs0Oe1b14IprStNdfuVyZsSJTeFm203S9zlDgyo
+# Cut0MvKGweSYZBTqm0bbeA66agXU6axq4Ars4o5EkiW5zOk7TkjilAAuWyjOKXeF
+# zfb8oy3Zc3EbEWn7M2ERAhcP4+HhEXjHvQGrypjFjnIz5ctY6V9RZzGT4bwDol3y
+# ROrXejY9CKBmF5iT2kYP3ZDlfqTiogjPYbhmqBkGwRCw5uqM7qGCAjAwggIsBgkq
+# hkiG9w0BCQYxggIdMIICGQIBATCBhjByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMM
+# RGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQD
+# EyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgVGltZXN0YW1waW5nIENBAhANQkrg
+# vjqI/2BAIc4UAPDdMA0GCWCGSAFlAwQCAQUAoGkwGAYJKoZIhvcNAQkDMQsGCSqG
+# SIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwMzEzMjMzNzE2WjAvBgkqhkiG9w0B
+# CQQxIgQg1/XFO2QiD1Y7RSB8M2RgYKkZPHh01sM8Urm4JpszABIwDQYJKoZIhvcN
+# AQEBBQAEggEAdauuUH3OkGo8vamtkZRFRNuMWraosJX4c6wgtedCHOkhZCulTDin
+# 5zIcShKtSTKi5PVNh4JE8etKy17LRZ1A9z+P1w64IKTv07AtGlqn4nfkbX89q8q7
+# kjPzWYRCsY4hlx0ToJVUGAGxsU3BZ+I0zh9SXleO9VliusKIdCskDnGQtdxmaWlS
+# LMpanXBBlZFj2drHhdF5o/rgMNc7dkT2TkxRRdgNyOFDrIjgbb54u7NxdN157L+V
+# EGV0Dok4QBGfIiTXHHFQy9Kry2Ru7rtpnQKPtg0cGsGyjbeDVjzM5B+RGYBw871+
+# DjpzDxiMgySn/B1pJTut8Mk9sZtLhIEL/Q==
+# SIG # End signature block
